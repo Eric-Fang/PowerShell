363357ebee326843374a6dca2fadef4fd2d82b77
Eric Fang <fangdahai@gmail.com>
2017-04-13 14:16:06 +1000

    Remove Invalid Event Receivers by Keywords

---
+++ RemoveInvalidEventReceivers.ps1
@@ -0,0 +1,161 @@
+#http://get-spscripts.com/2011/06/removing-features-from-content-database.html
+
+$ver = $host | select version
+if ($ver.Version.Major -gt 1)  {$Host.Runspace.ThreadOptions = "ReuseThread"}
+Add-PSSnapin Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue
+cls
+
+Set-StrictMode -Version Latest
+#$ErrorActionPreference="stop"
+
+$LogFile = "E:\Log_RemoveInvalidEventReceivers." + (Get-Date).ToString("yyyyMMdd-HHmmss") + ".txt"
+
+function Write-Log{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [ValidateNotNullOrEmpty()]
+        [Alias("LogContent")]
+        [string]$Message,
+
+        [Parameter(Mandatory=$false)]
+        [ValidateSet("Error","Warn","Info","HighLight")]
+        [string]$Level="Info"
+    )
+
+    Begin{
+        $VerbosePreference = 'Continue'
+    }
+    Process{
+        #if (!(Test-Path $LogFile)) {
+        #    Write-Verbose "Creating $LogFile."
+        #    $NewLogFile = New-Item $LogFile -Force -ItemType File
+        #}
+
+        $FormattedDate = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+
+        switch ($Level) {
+            'Error' {
+                $LevelText = 'ERROR:'
+				$MessageColor = [System.ConsoleColor]::Red
+            }
+            'Warn' {
+                $LevelText = 'WARNING:'
+				$MessageColor = [System.ConsoleColor]::Yellow
+            }
+            'Info' {
+                $LevelText = 'INFO:'
+				$MessageColor = [System.ConsoleColor]::DarkGreen
+            }
+            'HighLight' {
+                $LevelText = 'HIGHLIGHT:'
+				$MessageColor = [System.ConsoleColor]::Green
+            }
+        }
+        Write-Host $Message -f $MessageColor
+
+		$MessageContent = "$FormattedDate $LevelText $Message"
+        $MessageContent | Out-File -FilePath $LogFile -Append
+		#$opts = @{ForegroundColor=$MessageColor; BackgroundColor="black"; object=$MessageContent}
+		#Write-Log $opts
+    }
+    End{
+    }
+}
+
+function RemoveInvalidEventReceivers([string]$startSPSiteUrl, [string]$AssemblyName, [switch]$ReportOnly)
+{
+Write-Log "$(get-date -UFormat '%Y%m%d %H:%M:%S') - RemoveInvalidEventReceivers() begin... $startSPSiteUrl, ReportOnly=$ReportOnly"
+    $sites = @(Get-SPWebApplication -IncludeCentralAdministration | Get-SPSite -Limit ALL | ?{$_.ServerRelativeUrl -notmatch "Office_Viewing_Service_Cache"})
+    $SiteCount = $sites.count
+    $progressBarTitle = "RemoveInvalidEventReceivers(), SiteCount=$SiteCount, startSPSiteUrl=$startSPSiteUrl"
+    $i = 0
+    foreach($site in $sites){
+		$count = 0
+        $i++
+		Write-Progress -Activity "$progressBarTitle" -PercentComplete (($i/$SiteCount)*100) -Status "Working"
+
+		try{
+			$evenReceiverIds = @($site.EventReceivers | ?{ $_.Assembly -like "*$AssemblyName*" })
+			if ($evenReceiverIds.Count -gt 0){
+				$count += $evenReceiverIds.Count
+			}
+			$evenReceiverIds | %{
+				$er = $site.EventReceivers[$_.ID]
+				if ($ReportOnly){
+					Write-Log "site level: '$($er.Assembly)', $($er.Class), $($er.Type)" -Level HighLight
+				}
+				else{
+					Write-Log "Deleting site level: '$($er.Assembly)', $($er.Class), $($er.Type)" -Level HighLight
+					$er.Delete()
+					Write-Log "Deleted"
+				}
+				Write-Host ""
+			}
+			
+			foreach($web in $site.AllWebs){
+				foreach($ct in $web.ContentTypes){
+					$evenReceiverIds = @($ct.EventReceivers | ?{ $_.Assembly -like "*$AssemblyName*" })
+					if ($evenReceiverIds.Count -gt 0){
+						$count += $evenReceiverIds.Count
+					}
+					$evenReceiverIds | %{
+						$er = $ct.EventReceivers[$_.ID]
+						if ($ReportOnly){
+							Write-Log "site Content Type level: '$($er.Assembly)', $($er.Class), $($er.Type)" -Level HighLight
+						}
+						else{
+							Write-Log "Deleting site Content Type level: '$($er.Assembly)', $($er.Class), $($er.Type)" -Level HighLight
+							$er.Delete()
+							Write-Log "Deleted"
+						}
+						Write-Host ""
+					}
+				}
+				
+				$web.Dispose()
+			}
+			
+			foreach($web in $site.AllWebs){
+				foreach($list in $web.Lists){
+					$evenReceiverIds = @($list.EventReceivers | ?{ $_.Assembly -like "*$AssemblyName*" })
+					if ($evenReceiverIds.Count -gt 0){
+						$count += $evenReceiverIds.Count
+					}
+					$evenReceiverIds | %{
+						$er = $list.EventReceivers[$_.ID]
+						if ($ReportOnly){
+							Write-Log "list level: '$($er.Assembly)', $($er.Class), $($er.Type)" -Level HighLight
+						}
+						else{
+							Write-Log "Deleting list level: '$($er.Assembly)', $($er.Class), $($er.Type)" -Level HighLight
+							$er.Delete()
+							Write-Log "Deleted"
+						}
+						Write-Host ""
+					}
+				}
+				
+				$web.Dispose()
+			}
+			
+			if ($count -gt 0){
+				Write-Log "site.Url=$($site.Url), count=$count" -Level Warn
+			}
+		}
+		Catch [system.exception]{
+			$strTmp = [string]::Format("RemoveInvalidEventReceivers(), SiteUrl={0}, ex.Message={1}", $site.Url, $Error[0].Exception.Message)
+	Write-Log $strTmp -Level Error
+	Write-Log $_.Exception -Level Error
+		}
+		
+		$site.Dispose()
+    }
+}
+
+$_AssemblyName = "Microsoft.AnalysisServices.SPAddin"
+RemoveInvalidEventReceivers "" $_AssemblyName
+
+Write-Log "Finished! Press enter key to exit."
+# Read-Host
